71b4ce0d572d9e038397fa7c04ee3d499ec6f128
Vlad Liamzin <vlad.lyamzin@avenga.com>
2020-11-10 02:03:16 -0800

    Fix user-agent for google authorization service (#6)
    * Fix user-agent for google authorization service
    * Use platform service

--- src/browser-view-container.ts
+++ src/browser-view-container.ts
@@ -1,4 +1,4 @@
-import {BrowserView, shell} from "electron";
+import {BrowserView, shell, session, OnBeforeSendHeadersListenerDetails, BeforeSendResponse} from 'electron';
 import {environment} from './environment';
 import * as path from 'path';
 import {ContextMenu} from './context-menu';
@@ -24,7 +24,7 @@ export class BrowserViewContainer {
     BrowserViewContainer._windowRef = window;
     this._browserViewRef.setAutoResize( { width: true, height: true } )
     this._browserViewRef.webContents.loadURL(environment.serviceUrl);
-    this._browserViewRef.webContents.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36';
+    session.defaultSession.webRequest.onBeforeSendHeaders(BrowserViewContainer.overwriteUA)
     this.setBounds();
     this._browserViewRef.webContents.on("new-window", (event, url) => {
       shell.openExternal(url);
@@ -59,5 +59,24 @@ export class BrowserViewContainer {
     } else {
       return BrowserViewContainer._windowRef.isMenuBarVisible() ? 25 : 0;
     }
+  }
+
+  private static getUserAgent(): string {
+    if (Platform.isOSX()) {
+      return 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36';
+    } else if (Platform.isWin()) {
+      return 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36';
+    } else if (Platform.isLinux()){
+      return 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36';
+    }
+  }
+
+  private static overwriteUA(details: OnBeforeSendHeadersListenerDetails, callback: (beforeSendResponse: BeforeSendResponse) => void): void {
+    if (/accounts.google/.test(details.url)) {
+      details.requestHeaders['User-Agent'] = 'Chrome';
+    } else {
+      details.requestHeaders['User-Agent'] = BrowserViewContainer.getUserAgent();
+    }
+    callback({ cancel: false, requestHeaders: details.requestHeaders });
   }
 }
